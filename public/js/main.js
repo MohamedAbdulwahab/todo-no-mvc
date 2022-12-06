6ed8246cd679888e91aee3012dc12e60148db2b5
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-12-05 20:07:04 -0800

    added client side mark complete and mark uncomplete

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,17 @@
 const deleteBtn = document.querySelectorAll('.fa-trash');
+const item = document.querySelectorAll('.item span.uncomplete');
+const itemCompleted = document.querySelectorAll('.item span.completed');
 
 Array.from(deleteBtn).forEach((element) => {
 	element.addEventListener('click', deleteItem);
+});
+
+Array.from(item).forEach((element) => {
+	element.addEventListener('click', markComplete);
+});
+
+Array.from(itemCompleted).forEach((element) => {
+	element.addEventListener('click', markUnComplete);
 });
 
 async function deleteItem() {
@@ -17,25 +27,46 @@ async function deleteItem() {
 		const itemDeleted = await response.json();
 		console.log(itemDeleted);
 		location.reload();
-		
+
 	} catch(error) {
 		console.log(error);
 	}
 }
 
-// const itemText = this.parentNode.childNodes[1].innerText
-// try{
-// 	const response = await fetch('deleteItem', {
-// 		method: 'delete',
-// 		headers: {'Content-Type': 'application/json'},
-// 		body: JSON.stringify({
-// 		  'itemFromJS': itemText
-// 		})
-// 	  })
-// 	const data = await response.json()
-// 	console.log(data)
-// 	location.reload()
+async function markComplete() {
+	const itemText = this.parentNode.childNodes[1].innerText;
+	try {
+		const response = await fetch('/markComplete', {
+			method: 'put',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				'itemFromJS': itemText
+			})
+		});
+		const markedComplete = await response.json();
+		console.log('Marked Complete from client /markComplete');
+		location.reload();
 
-// }catch(err){
-// 	console.log(err)
-// }
+	} catch(error) {
+		console.log(error);
+	}
+}
+
+async function markUnComplete() {
+	const itemText = this.parentNode.childNodes[1].innerText;
+	try {
+		const response = await fetch('/markUncomplete', {
+			method: 'put',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				'itemFromJS': itemText
+			})
+		});
+		const markedunComplete = await response.json();
+		console.log('Marked Uncomplete from client /markUncomplete');
+		location.reload();
+		
+	} catch(error) {
+		console.log(error);
+	}
+}
